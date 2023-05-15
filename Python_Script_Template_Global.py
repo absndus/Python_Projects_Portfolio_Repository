4ce4e1ee5144e3ff9f74c3256670ccce529d37f3
Albert Schultz <absndus@outlook.com>
2023-05-15 08:18:14 -0500

    Copied the Python Script Template script to this project folder.

---
+++ Python_Script_Template_Global.py
@@ -0,0 +1,13 @@
+#File: <Enter Title Here That Matches the Filename>.py
+#Author: Albert Schultz
+#Date: <Enter date as MM/dd/yyyy>
+#Version: <Enter version number as 1.00>
+#Descriptions: This is a template Python Script that was standardized in the data science and programming workflows.
+
+#Import the required library modules.
+
+#Create required variables needed for this script to run.
+
+#Perform the <enter the execution code function summary>.
+
+#Present the variables to the console as an output. 
