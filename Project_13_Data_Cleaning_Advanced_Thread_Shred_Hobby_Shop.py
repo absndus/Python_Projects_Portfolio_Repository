72715e020b6aaed46c3b739aae8fa4bbda8a63d1
Albert Schultz <absndus@outlook.com>
2023-05-16 20:50:57 -0500

    Created the Project 13 Cleaning Advanced Raw Data for the fictional Hobby shop store for the team to make sense of the data.

---
+++ Project_13_Data_Cleaning_Advanced_Thread_Shred_Hobby_Shop.py
@@ -0,0 +1,191 @@
+#File: Project_13_Data_Cleaning_Advanced_Thread_Shope_Hobby_Shope.py
+#Author: Albert Schultz
+#Date: 05/16/2023
+#Version: 1.00
+#Descriptions: This project script goes through the process of importing the raw transaction data set that required cleaning to make the information presentable to the store teams.
+
+#Import library modules.
+import math as m
+import pandas as pd
+
+#Review the data set below of the raw transactions from the fictional hobby store, Thread Shop.
+daily_sales = \
+"""Edith Mcbride   ;,;$1.21   ;,;   white ;,; 
+09/15/17   ,Herbert Tran   ;,;   $7.29;,; 
+white&blue;,;   09/15/17 ,Paul Clarke ;,;$12.52 
+;,;   white&blue ;,; 09/15/17 ,Lucille Caldwell   
+;,;   $5.13   ;,; white   ;,; 09/15/17,
+Eduardo George   ;,;$20.39;,; white&yellow 
+;,;09/15/17   ,   Danny Mclaughlin;,;$30.82;,;   
+purple ;,;09/15/17 ,Stacy Vargas;,; $1.85   ;,; 
+purple&yellow ;,;09/15/17,   Shaun Brock;,; 
+$17.98;,;purple&yellow ;,; 09/15/17 , 
+Erick Harper ;,;$17.41;,; blue ;,; 09/15/17, 
+Michelle Howell ;,;$28.59;,; blue;,;   09/15/17   , 
+Carroll Boyd;,; $14.51;,;   purple&blue   ;,;   
+09/15/17   , Teresa Carter   ;,; $19.64 ;,; 
+white;,;09/15/17   ,   Jacob Kennedy ;,; $11.40   
+;,; white&red   ;,; 09/15/17, Craig Chambers;,; 
+$8.79 ;,; white&blue&red   ;,;09/15/17   , Peggy Bell;,; $8.65 ;,;blue   ;,; 09/15/17,   Kenneth Cunningham ;,;   $10.53;,;   green&blue   ;,; 
+09/15/17   ,   Marvin Morgan;,;   $16.49;,; 
+green&blue&red   ;,;   09/15/17 ,Marjorie Russell 
+;,; $6.55 ;,;   green&blue&red;,;   09/15/17 ,
+Israel Cummings;,;   $11.86   ;,;black;,;  
+09/15/17,   June Doyle   ;,;   $22.29 ;,;  
+black&yellow ;,;09/15/17 , Jaime Buchanan   ;,;   
+$8.35;,;   white&black&yellow   ;,;   09/15/17,   
+Rhonda Farmer;,;$2.91 ;,;   white&black&yellow   
+;,;09/15/17, Darren Mckenzie ;,;$22.94;,;green 
+;,;09/15/17,Rufus Malone;,;$4.70   ;,; green&yellow 
+;,; 09/15/17   ,Hubert Miles;,;   $3.59   
+;,;green&yellow&blue;,;   09/15/17   , Joseph Bridges  ;,;$5.66   ;,; green&yellow&purple&blue 
+;,;   09/15/17 , Sergio Murphy   ;,;$17.51   ;,;   
+black   ;,;   09/15/17 , Audrey Ferguson ;,; 
+$5.54;,;black&blue   ;,;09/15/17 ,Edna Williams ;,; 
+$17.13;,; black&blue;,;   09/15/17,   Randy Fleming;,;   $21.13 ;,;black ;,;09/15/17 ,Elisa Hart;,; $0.35   ;,; black&purple;,;   09/15/17   ,
+Ernesto Hunt ;,; $13.91   ;,;   black&purple ;,;   
+09/15/17,   Shannon Chavez   ;,;$19.26   ;,; 
+yellow;,; 09/15/17   , Sammy Cain;,; $5.45;,;   
+yellow&red ;,;09/15/17 ,   Steven Reeves ;,;$5.50   
+;,;   yellow;,;   09/15/17, Ruben Jones   ;,; 
+$14.56 ;,;   yellow&blue;,;09/15/17 , Essie Hansen;,;   $7.33   ;,;   yellow&blue&red
+;,; 09/15/17   ,   Rene Hardy   ;,; $20.22   ;,; 
+black ;,;   09/15/17 ,   Lucy Snyder   ;,; $8.67   
+;,;black&red  ;,; 09/15/17 ,Dallas Obrien ;,;   
+$8.31;,;   black&red ;,;   09/15/17,   Stacey Payne 
+;,;   $15.70   ;,;   white&black&red ;,;09/15/17   
+,   Tanya Cox   ;,;   $6.74   ;,;yellow   ;,; 
+09/15/17 , Melody Moran ;,;   $30.84   
+;,;yellow&black;,;   09/15/17 , Louise Becker   ;,; 
+$12.31 ;,; green&yellow&black;,;   09/15/17 ,
+Ryan Webster;,;$2.94 ;,; yellow ;,; 09/15/17 
+,Justin Blake ;,; $22.46   ;,;white&yellow ;,;   
+09/15/17,   Beverly Baldwin ;,;   $6.60;,;   
+white&yellow&black ;,;09/15/17   ,   Dale Brady   
+;,;   $6.27 ;,; yellow   ;,;09/15/17 ,Guadalupe Potter ;,;$21.12   ;,; yellow;,; 09/15/17   , 
+Desiree Butler ;,;$2.10   ;,;white;,; 09/15/17  
+,Sonja Barnett ;,; $14.22 ;,;white&black;,;   
+09/15/17, Angelica Garza;,;$11.60;,;white&black   
+;,;   09/15/17   ,   Jamie Welch   ;,; $25.27   ;,; 
+white&black&red ;,;09/15/17   ,   Rex Hudson   
+;,;$8.26;,;   purple;,; 09/15/17 ,   Nadine Gibbs 
+;,;   $30.80 ;,;   purple&yellow   ;,; 09/15/17   , 
+Hannah Pratt;,;   $22.61   ;,;   purple&yellow   
+;,;09/15/17,Gayle Richards;,;$22.19 ;,; 
+green&purple&yellow ;,;09/15/17   ,Stanley Holland 
+;,; $7.47   ;,; red ;,; 09/15/17 , Anna Dean;,;$5.49 ;,; yellow&red ;,;   09/15/17   ,
+Terrance Saunders ;,;   $23.70  ;,;green&yellow&red 
+;,; 09/15/17 ,   Brandi Zimmerman ;,; $26.66 ;,; 
+red   ;,;09/15/17 ,Guadalupe Freeman ;,; $25.95;,; 
+green&red ;,;   09/15/17   ,Irving Patterson 
+;,;$19.55 ;,; green&white&red ;,;   09/15/17 ,Karl Ross;,;   $15.68;,;   white ;,;   09/15/17 , Brandy Cortez ;,;$23.57;,;   white&red   ;,;09/15/17, 
+Mamie Riley   ;,;$29.32;,; purple;,;09/15/17 ,Mike Thornton   ;,; $26.44 ;,;   purple   ;,; 09/15/17, 
+Jamie Vaughn   ;,; $17.24;,;green ;,; 09/15/17   , 
+Noah Day ;,;   $8.49   ;,;green   ;,;09/15/17   
+,Josephine Keller ;,;$13.10 ;,;green;,;   09/15/17 ,   Tracey Wolfe;,;$20.39 ;,; red   ;,; 09/15/17 ,
+Ignacio Parks;,;$14.70   ;,; white&red ;,;09/15/17 
+, Beatrice Newman ;,;$22.45   ;,;white&purple&red 
+;,;   09/15/17, Andre Norris   ;,;   $28.46   ;,;   
+red;,;   09/15/17 ,   Albert Lewis ;,; $23.89;,;   
+black&red;,; 09/15/17,   Javier Bailey   ;,;   
+$24.49   ;,; black&red ;,; 09/15/17   , Everett Lyons ;,;$1.81;,;   black&red ;,; 09/15/17 ,   
+Abraham Maxwell;,; $6.81   ;,;green;,;   09/15/17   
+,   Traci Craig ;,;$0.65;,; green&yellow;,; 
+09/15/17 , Jeffrey Jenkins   ;,;$26.45;,; 
+green&yellow&blue   ;,;   09/15/17,   Merle Wilson 
+;,;   $7.69 ;,; purple;,; 09/15/17,Janis Franklin   
+;,;$8.74   ;,; purple&black   ;,;09/15/17 ,  
+Leonard Guerrero ;,;   $1.86   ;,;yellow  
+;,;09/15/17,Lana Sanchez;,;$14.75   ;,; yellow;,;   
+09/15/17   ,Donna Ball ;,; $28.10  ;,; 
+yellow&blue;,;   09/15/17   , Terrell Barber   ;,; 
+$9.91   ;,; green ;,;09/15/17   ,Jody Flores;,; 
+$16.34 ;,; green ;,;   09/15/17,   Daryl Herrera 
+;,;$27.57;,; white;,;   09/15/17   , Miguel Mcguire;,;$5.25;,; white&blue   ;,;   09/15/17 ,   
+Rogelio Gonzalez;,; $9.51;,;   white&black&blue   
+;,;   09/15/17   ,   Lora Hammond ;,;$20.56 ;,; 
+green;,;   09/15/17,Owen Ward;,; $21.64   ;,;   
+green&yellow;,;09/15/17,Malcolm Morales ;,;   
+$24.99   ;,;   green&yellow&black;,; 09/15/17 ,   
+Eric Mcdaniel ;,;$29.70;,; green ;,; 09/15/17 
+,Madeline Estrada;,;   $15.52;,;green;,;   09/15/17 
+, Leticia Manning;,;$15.70 ;,; green&purple;,; 
+09/15/17 ,   Mario Wallace ;,; $12.36 ;,;green ;,; 
+09/15/17,Lewis Glover;,;   $13.66   ;,;   
+green&white;,;09/15/17,   Gail Phelps   ;,;$30.52   
+;,; green&white&blue   ;,; 09/15/17 , Myrtle Morris 
+;,;   $22.66   ;,; green&white&blue;,;09/15/17"""
+
+#Perform splits based on the comma.
+daily_sales_replaced = daily_sales.replace(';,;',';')
+
+#Now, replace all of the , with the ;.
+daily_sales_replaced_2 = daily_sales_replaced.replace(',', ';')
+
+#Now, split by ; symbol.
+daily_sales_replaced_3 = daily_sales_replaced_2.split(';')
+#print(list(daily_sales_replaced_3)) #See the list modified.
+#print(len(list(daily_sales_replaced_3))) #Numberes of items in the variable.
+
+#Remove the \n characters and replace with "".
+daily_sales_replaced_4 = [string.replace("\n", "") for string in daily_sales_replaced_3]
+#print(list(daily_sales_replaced_4))
+
+#Replace & with ' ' in the data set.
+daily_sales_replaced_5 = [string.replace("&", " ") for string in daily_sales_replaced_4]
+#print(list(daily_sales_replaced_5))
+
+#Replace the dollar sign, $ with a ''.
+daily_sales_replaced_6 = [string.replace("$", "") for string in daily_sales_replaced_5]
+#print(list(daily_sales_replaced_6))
+
+#Remove the extra spaces front and back.
+daily_sales_replaced_7 = [string.replace("   ","") for string in daily_sales_replaced_6]
+#print(list(daily_sales_replaced_7))
+#print(len(list(daily_sales_replaced_7)))
+
+#Remove the empty space on the FRONT and LEFTside of each value if detected.
+daily_sales_replaced_8 = [string.lstrip() for string in daily_sales_replaced_7]
+#print(list(daily_sales_replaced_8))
+
+daily_sales_replaced_9 = [string.rstrip() for string in daily_sales_replaced_8]
+#print(list(daily_sales_replaced_9))
+
+#Get the names through iterations through the variable, daily_sales_replaced_9.
+customers = []
+for i in range(0, len(daily_sales_replaced_9), 4):
+    customers.append(daily_sales_replaced_9[i])
+#print(list(name))
+
+#Go through the iteration to get the dollar amount per transaction.
+sales = []
+for i in range(1, len(daily_sales_replaced_9), 4):
+    sales.append(daily_sales_replaced_9[i])
+
+#print(list(cost))
+
+#Now, pull the items into a item list from the daily_sales_replaced_9.
+thread_sold = []
+for i in range(2, len(daily_sales_replaced_9), 4):
+    thread_sold.append(daily_sales_replaced_9[i])
+
+#print(list(item))
+
+#Now, pull date out of every 3rd indexes from the daily_sales_replaced_9.
+dates = []
+for i in range(3, len(daily_sales_replaced_9), 4):
+    dates.append(daily_sales_replaced_9[i])
+
+#print(list(date))
+
+#Zip the variable lists together; customers, sales, and dates.
+daily_sales_cleaned = zip(customers, sales, dates)
+print(list(daily_sales_cleaned))
+
+#To view the individual item in the list. Convert the zip list to list.
+items = [list(i) for i in zip(customers, sales, dates)]
+print(items[1])
+
+#Convert the new cost list variable into a float data type from string by 2 decimal places.
+sales_total = "{:.2f}".format(sum(list(map(float, sales)))) #Max was up to two decimal point.
+print(f"The total transaction total for the Shred Hobby Shop is, ${sales_total}.")  #Can confirm that the list has been converted and totaled up to $1498.74 for all of the transaction.
